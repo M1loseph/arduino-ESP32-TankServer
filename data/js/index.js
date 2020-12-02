7ec30a4c2c28c82c7c15a8bcfffe2156868c1a31
M1loseph <milosz1998@onet.eu>
2020-12-02 10:45:47 +0100

    leds

--- data/js/index.js
+++ data/js/index.js
@@ -34,8 +34,8 @@ document.addEventListener("DOMContentLoaded", () => {
     document.querySelector("#button-engines").onclick = () => {
         document.querySelector("#dial-engines").classList.remove("hidden");
     };
-    document.querySelector("#button-led").onclick = () => {
-        document.querySelector("#dial-led").classList.remove("hidden");
+    document.querySelector("#button-leds").onclick = () => {
+        document.querySelector("#dial-leds").classList.remove("hidden");
     };
     document.querySelector("#button-sd").onclick = () => {
         document.querySelector("#dial-sd").classList.remove("hidden");
@@ -48,39 +48,50 @@ document.addEventListener("DOMContentLoaded", () => {
         };
     });
 
-    // music dial
-    document.querySelectorAll(".mp3").forEach((elem) => {
-        elem.onclick = () => {
-            sendWS({ controller: "mp3", command: elem.id });
-        }
-    });
-
-    // arm dial
-    let rangeBullet = document.getElementById("base-bullet");
-
-    document.querySelectorAll('.arm-slider').forEach(slider => {
+    // all sliders
+    document.querySelectorAll("input[type='range']").forEach(slider => {
         slider.addEventListener("input", () => {
             slider.previousElementSibling.innerHTML = slider.value;
         }, false);
+    });
 
-        slider.addEventListener("touchend", () => {
-            sendWS({ controller: "arm", command: "angle", angle: slider.value, servo: slider.id })
+    // music dial
+    document.querySelectorAll(".mp3").forEach(button => {
+        button.onclick = () => {
+            sendWS({ controller: "mp3", command: button.id });
+        };
+    });
+
+    // arm dial
+    document.querySelectorAll('.arm-slider').forEach(slider => {
+        slider.addEventListener("change", () => {
+            sendWS({ controller: "arm", command: "angle", angle: slider.value, servo: slider.id });
         });
     });
 
-    function sendColor() {
-        let hexColor = ledColorPicker.value;
-        // remove 1st char
+    // leds dial
+    document.querySelectorAll(".leds").forEach(button => {
+        button.onclick = () => {
+            sendWS({ controller: "leds", command: button.id });
+        };
+    });
+
+    document.querySelector("#set_interval").addEventListener("change", event => {
+        sendWS({ controller: "leds", command: "set_interval", interval: event.target.value });
+    });
+
+    document.querySelector("#set_custom_color").addEventListener("change", (event) => {
+        let hexColor = event.target.value;
         hexColor = hexColor.substring(1);
 
-        let RGBHex = hexColor.match(/.{1,2}/g);
-        let RGB = [
+        const RGBHex = hexColor.match(/.{1,2}/g);
+        const RGB = [
             parseInt(RGBHex[0], 16),
             parseInt(RGBHex[1], 16),
             parseInt(RGBHex[2], 16),
         ];
         sendWS({ controller: "leds", command: "set_custom_color", colors: [RGB[0], RGB[1], RGB[2]] });
-    }
+    }, false);
 
     function sendSpeed() {
         sendWS({ controller: "engines", command: "speed", speed: speedSlider.value });

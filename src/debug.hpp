03fcac9f4f1b89abb629c0c93b420e60820a2aea
M1loseph <milosz1998@onet.eu>
2020-12-03 21:18:08 +0100

    json debug

--- src/debug.hpp
+++ src/debug.hpp
@@ -7,12 +7,14 @@
 #define LOG_NL(messege) Serial.println(messege);
 #define LOG(messege) Serial.print(messege);
 #define LOG_F(...) Serial.printf(__VA_ARGS__);
+#define LOG_JSON_PRETTY(json) serializeJsonPretty(json, Serial); LOG('\n');
 
 #else
 
 #define LOG_NL(messege)
 #define LOG(messege)
 #define LOG_F(...)
+#define LOG_JSON_PRETTY(json) serializeJsonPretty(json, Serial); LOG('\n');
 
 #endif // SMART_TANK_DEBUG
 

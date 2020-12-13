43501428c7038b220766955d7b6db069507da4f3
M1loseph <milosz1998@onet.eu>
2020-12-13 21:40:46 +0100

    scripts

--- src/controllers/sd_controller.cpp
+++ src/controllers/sd_controller.cpp
@@ -22,20 +22,28 @@
 namespace json_parser
 {
     sd_controller::sd_controller() : controller("sd", JSON_OBJECT_SIZE(2)),
-                                     _execute(false)
+                                     _execute(false),
+                                     _last_log(0),
+                                     _last_executed(0),
+                                     _json(nullptr)
     {
+    }
+
+    sd_controller::~sd_controller()
+    {
+        delete_json();
     }
 
     bool sd_controller::initialize()
     {
         bool succ = SD.begin(CHIP_SELECT);
-        if(succ)
+        if (succ)
         {
-            if(!SD.exists(LOG_FILE))
+            if (!SD.exists(LOG_FILE))
             {
-                LOG_SD_F("[%s] creating logs file...", _name)
+                LOG_SD_F("[%s] creating logs file...\n", _name)
                 auto temp_file = SD.open(LOG_FILE);
-                if(temp_file)
+                if (temp_file)
                     temp_file.close();
             }
         }
@@ -57,11 +65,13 @@ namespace json_parser
         {
             if (!_file_to_execute.isEmpty() && !_file)
             {
+                delete_json();
                 LOG_SD_F("[%s] opening file to execute: %s\n", _name, _file_to_execute.c_str())
                 _file = SD.open(_file_to_execute);
                 _file_to_execute.clear();
+                _last_executed = millis();
 
-                if(_file)
+                if (_file)
                 {
                     LOG_SD_F("[%s] opened file successfully\n", _name)
                 }
@@ -71,24 +81,37 @@ namespace json_parser
                 }
             }
 
-            if(_file)
+            if (_file)
             {
-                DynamicJsonDocument *json = new DynamicJsonDocument(512);
-                LOG_SD_F("[%s] peek before read char: %c int: %d\n", _name, _file.peek(), _file.peek())
-                auto error = deserializeJson(*json, _file);
-
-                if(!error)
+                // there is a json -> needs to be handled (maybe waiting or smth)
+                if (_json)
                 {
-                    LOG_SD_JSON_PRETTY(*json)
+                    handle_current_json();
                 }
-
-                if (error || !global_queue::queue.push(&json))
+                else
                 {
-                    LOG_SD_F("[%s] closing file after execution\n", _name)
-                    LOG_SD_F("[%s] error: %s (might be queue fault)", _name, error.c_str())
-                    delete json;
-                    _file.close();
-                    _execute = false;
+                    // create new json from file
+                    _json = new DynamicJsonDocument(512);
+                    LOG_SD_F("[%s] peek int: %d\n", _name, _file.peek())
+                    auto error = deserializeJson(*_json, _file);
+
+                    if (error)
+                    {
+                        LOG_SD_F("[%s] error: %s\n", _name, error.c_str())
+                        delete_json();
+                        _file.close();
+                        _execute = false;
+                    }
+                    else if(!_json->containsKey(TIME_KEY))
+                    {
+                        LOG_SD_F("[%s] error, not time key in script\n", _name);
+                        delete_json();
+                    }
+                    else
+                    {
+                        LOG_SD_JSON_PRETTY(*_json)
+                        handle_current_json();
+                    }
                 }
             }
             else
@@ -98,46 +121,80 @@ namespace json_parser
         }
     }
 
-    bool sd_controller::handle(const JsonObject &json)
+    void sd_controller::handle_current_json()
     {
-        if (json.containsKey(COMMAND_KEY) && !strcmp(json[COMMAND_KEY].as<const char *>(), EXECUTE))
+        uint32_t interval = (*_json)[TIME_KEY];
+        if(millis() - _last_executed >= interval)
         {
-            LOG_SD_F("[%s] recived execute command\n", _name)
-            if (json.containsKey(FILE_KEY))
+            if(global_queue::queue.push(&_json))
             {
-                _file_to_execute = json[FILE_KEY].as<String>();
-                _file.close();
-                _execute = true;
-                return true;
-            }
-        }
-        else if (!json.containsKey(TIME_KEY))
-        {
-            _file.close();
-
-            _file = SD.open(LOG_FILE, "a");
-            if (_file)
-            {
-                json[TIME_KEY] = millis();
-                WriteBufferingStream bufferedFile(_file, 64);
-                serializeJson(json, _file);
-                bufferedFile.write('\n');
-                bufferedFile.flush();
-                _file.close();
-                LOG_SD_F("[%s] serialized JSON\n", _name)
-                return true;
+                LOG_SD_F("[%s] sent json\n", _name)
+                _json = nullptr;
             }
             else
             {
-                LOG_SD_F("[%s] unable to open the file\n", _name)
+                LOG_SD_F("[%s] sending error, trying again\n", _name)
+            }
+        }
+    }
+
+    bool sd_controller::handle(const JsonObject &json)
+    {
+        if (!json.containsKey(TIME_KEY))
+        {
+            if (json.containsKey(COMMAND_KEY) && !strcmp(json[COMMAND_KEY].as<const char *>(), EXECUTE))
+            {
+                LOG_SD_F("[%s] recived execute command\n", _name)
+                if (json.containsKey(FILE_KEY))
+                {
+                    _file_to_execute = json[FILE_KEY].as<String>();
+                    _file.close();
+                    _execute = true;
+                    return true;
+                }
+            }
+            else
+            {
+                LOG_SD_F("[%s] logging message\n", _name)
+                _file.close();
+
+                _file = SD.open(LOG_FILE, "a");
+                if (_file)
+                {
+                    auto time_point = millis();
+                    json[TIME_KEY] = time_point - _last_log;
+                    _last_log = time_point;
+
+                    WriteBufferingStream bufferedFile(_file, 64);
+                    serializeJson(json, _file);
+                    bufferedFile.write('\n');
+                    bufferedFile.flush();
+                    _file.close();
+                    LOG_SD_F("[%s] serialized JSON\n", _name)
+                    return true;
+                }
+                else
+                {
+                    LOG_SD_F("[%s] unable to open the file\n", _name)
+                }
             }
         }
         else
         {
-            LOG_SD_F("[%s] unable to handle\n", _name)
+            LOG_SD_F("[%s] not handling to prevent loop\n", _name)
         }
 
         return false;
+    }
+
+    void sd_controller::delete_json()
+    {
+        LOG_SD_F("[%s] deleting json\n", _name)
+        if (_json)
+        {
+            delete _json;
+            _json = nullptr;
+        }
     }
 
     DynamicJsonDocument sd_controller::retrive_data()
--- src/controllers/sd_controller.hpp
+++ src/controllers/sd_controller.hpp
@@ -7,19 +7,21 @@ namespace json_parser
     class sd_controller final : public controller
     {
     public:
-
         explicit sd_controller();
+        ~sd_controller();
         bool initialize() override;
         void update() override;
         DynamicJsonDocument retrive_data() override;
 
     private:
         bool can_handle(const JsonObject &json) const override;
-        bool handle(const JsonObject& json) override;
+        bool handle(const JsonObject &json) override;
+        void handle_current_json();
+        void delete_json();
 
         static constexpr uint8_t CHIP_SELECT = 5U;
-        static constexpr const char* EXECUTE = "execute";
-        static constexpr const char* FILE_KEY = "file";
+        static constexpr const char *EXECUTE = "execute";
+        static constexpr const char *FILE_KEY = "file";
 
         static constexpr const char *LOG_FILE = "/logs.txt";
 
@@ -28,5 +30,9 @@ namespace json_parser
         File _file;
         bool _execute;
         String _file_to_execute;
+
+        unsigned long _last_log;
+        unsigned long _last_executed;
+        DynamicJsonDocument *_json;
     };
-} // namespace sd_card
+} // namespace json_parser
